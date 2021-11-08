bb5a3de2d234d45c77456a417156d3b646cf15f1
Linas Smita <linassmita@Linass-MacBook-Air.local>
2021-11-08 13:24:55 +0000

    implemented lighhouse

---
+++ One-Page Website/opw.js
@@ -0,0 +1,23 @@
+const lightboxBg = document.createElement('div');
+lightboxBg.id = 'lightboxBg';
+document.body.appendChild(lightboxBg);
+
+const images = document.querySelectorAll("img");
+
+images.forEach(image => {
+  image.addEventListener("click", e => {
+    lightboxBg.classList.add('active');
+
+    const lightboxImg = document.createElement('img');
+    lightboxImg.src = image.src;
+    while(lightboxBg.firstChild){
+      lightboxBg.removeChild(lightboxBg.firstChild)
+    }
+    lightboxBg.appendChild(lightboxImg);
+  })
+})
+
+lightboxBg.addEventListener("click", e => {
+  if (e.target !== e.currentTarget) return;
+  lightboxBg.classList.remove('active');
+})
